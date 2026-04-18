9daf2d6cc22a2ac032070addb1bd5f923534520a
taha-iqbal-dev <tahaiqbaldev@gmail.com>
2026-04-18 15:50:01 +0500

    Added basics of variables and data types

---
+++ variables_datatypes/basics.py
@@ -0,0 +1,28 @@
+# Basic Data Types and Variables
+
+#integer- used to store whole numbers
+age = 20
+
+#float- used to store decimal numbers
+height = 5.8
+
+#string- used to store text data (Single characters also considered string in PYTHON)
+grade = 'A'
+
+#string- used to store text data
+name = "Taha Iqbal"
+
+#Boolean- used to store logical values True/False
+is_student = True
+
+#Printing data from variables
+
+print("My name is", name)
+print("I'm", age, "years old")
+print("My height is", height, "feet")
+print("Am I a student?", is_student)
+print("My grade is", grade)
+
+#More enhanced way of printing formated data
+print("\n---Profile Summary---")
+print(f"My name is {name}. I'm {age} years old. My height is {height} feet. Am I a student? {is_student}. My grade is {grade}.")
