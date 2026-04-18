15bffa929ca6cdfdb4a853f02ba7a0fc3eb45550
taha-iqbal-dev <tahaiqbaldev@gmail.com>
2026-04-18 23:40:58 +0500

    Add basic input/output with type casting examples

---
+++ input_output/01basic_io.py
@@ -0,0 +1,15 @@
+# Basic Input/Output
+
+#Input
+name = input("Enter your name: ") 
+# input() always return data as a string
+# Use type casting for (int, float) to convert when needed
+
+age = int(input("Enter your age: "))
+height = float(input("Enter your height: "))
+
+# Output
+print("\n---User information---")
+print("Your name is", name)
+print("Your age is", age)
+print("Your height is", height)
