5d4e30c011da73c1ccbd1b9e30e58ab7acaf969b
taha-iqbal-dev <tahaiqbaldev@gmail.com>
2026-04-20 22:27:52 +0500

    Implement basic if-else logic

---
+++ conditional_statments/01basic_conditional.py
@@ -0,0 +1,13 @@
+# Conditional Statments- shows results based on the given condition
+
+print("---CONDITIONAL STATMENT---")
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are an adult") # Indentation needed
+elif age >= 13:
+    print("Your are a teenager")
+else:
+    print("You are under age")
+
+# Note: invalid input (non-numeric) will cause an error
