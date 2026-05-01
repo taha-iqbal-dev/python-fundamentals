e485afc1c8f6672462badf61c782ce9ae63565e2
taha-iqbal-dev <tahaiqbaldev@gmail.com>
2026-05-01 14:13:58 +0500

    Add basic string operations including concatenation and length checking

---
+++ strings/01basic_str.py
@@ -0,0 +1,23 @@
+# String Operations in Python
+# Demonstrates basic printing, concatenation, and length calculation
+
+str1 = "My name is Taha. "
+str2 = "I'm a software engineering student. "
+str3 = "My goal is to become a successful software engineer. "
+
+# Basic operation
+print(str1)
+print(str2)
+print(str3)
+
+# Concatenation
+print("\n---Concatenation---")
+print(str1 + str2 + str3)
+
+# Length check
+print("\n---Length check---")
+print("Length of first string is: ", len(str1))
+print("Length of second string is: ", len(str2))
+print("Length of third string is: ", len(str3))
+
+# Note: len() counts all characters including spaces and punctuation
