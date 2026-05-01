1b865249a5416fb1cc2acfe493505adf65630ecd
taha-iqbal-dev <tahaiqbaldev@gmail.com>
2026-05-01 14:24:35 +0500

    Refine string indexing example with clearer output and formatting

---
+++ strings/02indexing.py
@@ -0,0 +1,9 @@
+# Indexing - accessing individual characters of a string using their position
+
+str1 = "TAHA IQBAL"
+# Index starts from 0 and goes up to length-1 (includes spaces as characters)
+
+print(str1)
+print("Character at Index 0: ", str1[0])
+print("Character at Index 4: ", str1[4]) # the index is space
+print("Character at Index -1 -- Last Character: ", str1[-1])
