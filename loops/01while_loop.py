5849a5615eeb0a31fce79c3dfb77447a24579f74
taha-iqbal-dev <tahaiqbaldev@gmail.com>
2026-05-02 11:11:24 +0500

    Add while loop examples with forward and reverse iteration

---
+++ loops/01while_loop.py
@@ -0,0 +1,19 @@
+# while condition :
+# statement to be execute (execution continue until condition turns false)
+
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+# Important: make sure loop variable changes, otherwise it can cause infinite loop
+
+print ("Loop ended")
+
+print("\n---REVERSE LOOP---")
+
+j = 10
+while j >= 1:
+    print(j)
+    j -= 1
+
+print("Loop Ended")
